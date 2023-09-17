56028b4f8e8f6aa51f2333736f97a4a71ff9ae99
carolyn-brodie <48027983+carolyn-brodie@users.noreply.github.com>
2023-09-17 14:19:17 -0500

    Fixed example

--- week4Monday/5.subtractSmallerFromBigger.py
+++ week4Monday/5.subtractSmallerFromBigger.py
@@ -6,19 +6,31 @@ number1 = int(input("Enter the first number: "))
 number2 = int(input("Enter the second number: "))
 count = 0
 
+multipler = 1
+
+if number2 < 0 and number1 >= 0:
+    multipler = -1
+
+elif number1 < 0 and number2 >= 0:
+    multipler = -1
+
+number1 = abs(number1)
+number2 = abs(number2)
 
 if number1 < number2:
     temp  = number1
     number1 = number2
     number2 = temp
- 
+
+
 result = number1
+if number2 == 0:
+    result = -1
+
+print(result, number2)
 while result >= number2:
     result -= number2
     count += 1
-
-
- 
-print(count)
+print(count * multipler)
     
 
